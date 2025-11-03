905f76e2387542da5918f118c6b82edcd79801be
abhishekkumar71 <151723904+abhishekkumar71@users.noreply.github.com>
2025-11-03 18:39:49 +0530

    modified chromium launch

--- pages/api/auth/twitter/connectToX.ts
+++ pages/api/auth/twitter/connectToX.ts
@@ -75,10 +75,12 @@ export default async function handler(
   let browser: any = null;
   try {
     console.log("[testLogin] Launching browser...");
-    browser = await chromium.launch({
-      headless: false,
-      slowMo: 250,
-      args: ["--disable-blink-features=AutomationControlled"],
+    const chromePath =
+      process.env.CHROME_PATH || "/app/.apt/opt/google/chrome/chrome";
+    const browser = await chromium.launch({
+      executablePath: chromePath,
+      args: ["--no-sandbox", "--disable-dev-shm-usage", "--disable-gpu"],
+      headless: true,
     });
 
     const context = await browser.newContext();
@@ -146,20 +148,16 @@ export default async function handler(
         },
       });
 
-      res
-        .status(200)
-        .json({
-          success: true,
-          message: "Login successful and cookies saved!",
-        });
+      res.status(200).json({
+        success: true,
+        message: "Login successful and cookies saved!",
+      });
     } else {
       console.log("Login not confirmed.");
-      res
-        .status(200)
-        .json({
-          success: false,
-          message: "Login attempt made, but not confirmed.",
-        });
+      res.status(200).json({
+        success: false,
+        message: "Login attempt made, but not confirmed.",
+      });
     }
 
     await browser.close();
